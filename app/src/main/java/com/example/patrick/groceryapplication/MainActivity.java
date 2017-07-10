7e7c1dc49cb18a5100b56399727cb31e4237538f
ptflinner <pflinne@calstatela.edu>
2017-07-10 09:35:00 -0700

    Begins read and write implementation

--- app/src/main/java/com/example/patrick/groceryapplication/MainActivity.java
+++ app/src/main/java/com/example/patrick/groceryapplication/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.patrick.groceryapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG="MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase mFirebaseDatabase=FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference=mFirebaseDatabase.getReference("message");
+
+        databaseReference.setValue("Hello, Database");
+
+        Log.d(TAG,"In Main");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value=dataSnapshot.getValue(String.class);
+                Log.d(TAG,"Value is: "+value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG,"Failed to read value. ", databaseError.toException());
+            }
+        });
     }
 }
