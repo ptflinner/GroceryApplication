af7832f6a4556085ad58dfb10ac5b1036de2a99c
ptflinner <pflinne@calstatela.edu>
2017-07-13 08:53:26 -0700

    Merges authentication with master

--- app/src/main/java/com/example/patrick/groceryapplication/LoginActivity.java
+++ app/src/main/java/com/example/patrick/groceryapplication/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.patrick.groceryapplication;
-import android.content.Context;
+
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
--- app/src/main/java/com/example/patrick/groceryapplication/MainActivity.java
+++ app/src/main/java/com/example/patrick/groceryapplication/MainActivity.java
@@ -11,13 +11,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
