77fde75afda23cda4bf014b19c27992ffbdc1efa
Barry <barryboayes17@gmail.com>
2017-07-27 18:45:16 -0700

    Allows for merge

--- app/src/main/java/com/example/patrick/groceryapplication/MyListAdapter.java
+++ app/src/main/java/com/example/patrick/groceryapplication/MyListAdapter.java
@@ -1,11 +1,14 @@
 package com.example.patrick.groceryapplication;
 
+
 import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 
 /**
  * Created by Barry on 7/26/2017.
@@ -13,27 +16,41 @@ import android.widget.TextView;
 
 public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ItemHolder>{
 
-    public MyListAdapter(){
+    public static final String TAG = "MyListAdapter";
+    private Cursor cursor;
+    private Context context;
+    private ItemClickListener listener;
 
-    }
 
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
 
-    @Override
-    public void onBindViewHolder(ItemHolder holder, int position) {
-        super.onBindViewHolder(holder, position);
+    public MyListAdapter(Cursor cursor, ItemClickListener listener){
+        this.cursor = cursor;
+        this.listener = listener;
     }
 
     @Override
     public ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.my_item_list, parent, false);
-        ItemHolder holder = new ItemHolder(view);
-        return holder;
+
+        View view = inflater.inflate(R.layout.my_lists, parent, false);
+        ItemHolder viewHolder = new ItemHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ItemHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cursor.getCount();
+    }
+
+    public interface ItemClickListener{
+        void onItemClick(Cursor cursor, int clickedItemIndex);
     }
 
     class ItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
@@ -47,15 +64,20 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ItemHolder
             view.setOnClickListener(this);
         }
 
-        public void bind(ItemHolder holder, int pos){
-            title.setText("My Big List of Colorful Dildos");
-            category.setText("Category: gaygay gay");
+        public void bind(int pos){
+            cursor.moveToPosition(pos);
+            title.setText(cursor.getString(cursor.getColumnIndex(Contract.COLUMN_NAME_LIST_NAME)))
+            category.setText(cursor.getString(cursor.getColumnIndex(Contract.COLUMN_NAME_LIST_CATEGORY)));
+            //
+
         }
 
         @Override
         public void onClick(View v) {
-            //goto grocerylist list thing. i think.
+            int pos = getAdapterPosition();
+            listener.onItemClick(cursor, pos);
         }
     }
 
 }
+
--- app/src/main/java/com/example/patrick/groceryapplication/MyListItemAdapter.java
+++ app/src/main/java/com/example/patrick/groceryapplication/MyListItemAdapter.java
@@ -1,6 +1,7 @@
 package com.example.patrick.groceryapplication;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,24 +10,49 @@ import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.List;
+
 /**
  * Created by Barry on 7/26/2017.
  */
 
-public class MyListItemAdapter {
-    public MyListItemAdapter(){
+public class MyListItemAdapter extends RecyclerView.Adapter<MyListItemAdapter.GroceryItemHolder>{
 
+    public static final String TAG = "MyListItemAdapter";
+    private Cursor cursor;
+    private Context context;
+
+
+    public MyListItemAdapter(Cursor cursor){
+        this.cursor = cursor;
     }
 
 
+    @Override
+    public GroceryItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
 
+        View view = inflater.inflate(R.layout.my_list_items, parent, false);
+        GroceryItemHolder holder = new GroceryItemHolder(view);
 
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(GroceryItemHolder holder, int position, List<Object> payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cursor.getCount();
+    }
 
     class GroceryItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         ImageView image;
         TextView name;
         TextView quantity;
-        TextView price;
         CheckBox checkBox;
 
         GroceryItemHolder(View view){
@@ -35,18 +61,18 @@ public class MyListItemAdapter {
             image = (ImageView) view.findViewById(R.id.grocery_image);
             name = (TextView) view.findViewById(R.id.grocery_name);
             quantity = (TextView) view.findViewById(R.id.grocery_quantity);
-            price = (TextView) view.findViewById(R.id.grocery_price);
             checkBox = (CheckBox) view.findViewById(R.id.checkBox);
             view.setOnClickListener(this);
         }
 
-        public void bind(MyListAdapter.ItemHolder holder, int pos){
-            //bind codecodecodecodecodecodecodecodecodecodecode
+        public void bind(int pos){
+            cursor.moveToPosition(pos);
+
+            name.setText(cursor.getString(cursor.getColumnIndex(Contract.COLUMN_NAME_ITEM_NAME)));
+            quantity.setText(cursor.getInt(cursor.getColumnIndex(Contract.COLUMN_NAME_QUANTITY)));
+
         }
 
-        @Override
-        public void onClick(View v) {
 
-        }
     }
 }
