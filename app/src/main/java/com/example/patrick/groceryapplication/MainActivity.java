7af49afe8a5f27a8023f0198e623248133b5923b
ptflinner <pflinne@calstatela.edu>
2017-07-29 13:46:47 -0700

    Begins work on a navigation drawer.

--- app/src/main/java/com/example/patrick/groceryapplication/MainActivity.java
+++ app/src/main/java/com/example/patrick/groceryapplication/MainActivity.java
@@ -6,11 +6,18 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.patrick.groceryapplication.fragments.*;
 import com.google.android.gms.auth.api.Auth;
@@ -26,12 +33,14 @@ public class MainActivity extends AppCompatActivity {
     private GoogleApiClient mGoogleApiClient;
     private BottomNavigationView mBottomNavView;
     private static final String TAG="MainActivity";
-
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private String[] drawerItems;
+    private ActionBarDrawerToggle mActionToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
@@ -40,8 +49,32 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         mGoogleApiClient.connect();
 
-        mBottomNavView=(BottomNavigationView) findViewById(R.id.bottom_navigation);
+        mDrawerLayout=(DrawerLayout) findViewById(R.id.navigation_drawer);
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        mDrawerList=(ListView) findViewById(R.id.left_drawer);
+        drawerItems=getResources().getStringArray(R.array.drawer_items);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_list,drawerItems));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mActionToggle=new ActionBarDrawerToggle(this,
+                mDrawerLayout,
+                R.string.open,
+                R.string.close
+        ){
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(getTitle());
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(getTitle());
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mBottomNavView=(BottomNavigationView) findViewById(R.id.bottom_navigation);
         mBottomNavView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener(){
 
@@ -82,11 +115,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen=mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.sign_out).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemThatWasClickedId=item.getItemId();
-        if(itemThatWasClickedId==R.id.sign_out){
-            signOutOfGoogle();
+        if(mActionToggle.onOptionsItemSelected(item)){
             return true;
+        }
+
+        switch (item.getItemId()){
+            case R.id.sign_out:
+                signOutOfGoogle();
+                break;
+            default:
+                return onOptionsItemSelected(item);
         }
         return onOptionsItemSelected(item);
     }
@@ -102,5 +149,18 @@ public class MainActivity extends AppCompatActivity {
                         finish();
                     }
                 });
+    }
+
+    private void selectItem(int pos){
+        if(pos==1){
+            signOutOfGoogle();
+        }
+    }
+    private class DrawerItemClickListener implements ListView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+            selectItem(pos);
+        }
     }
 }
