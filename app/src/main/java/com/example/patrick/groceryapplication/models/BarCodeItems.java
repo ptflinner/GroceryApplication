0dc762b92b5cac11dd6775aaf0f4ee2018af9388
simonlam <simonlam0007@yahoo.com>
2017-08-03 19:35:14 -0700

    NetWorkUtils and BarCodeIems model

---
+++ app/src/main/java/com/example/patrick/groceryapplication/models/BarCodeItems.java
@@ -0,0 +1,8 @@
+package com.example.patrick.groceryapplication.models;
+
+/**
+ * Created by Nomis on 8/3/2017.
+ */
+
+public class BarCodeItems {
+}
---
+++ app/src/main/java/com/example/patrick/groceryapplication/utils/NetworkUtils.java
@@ -0,0 +1,8 @@
+package com.example.patrick.groceryapplication.utils;
+
+/**
+ * Created by Nomis on 8/3/2017.
+ */
+
+public class NetworkUtils {
+}
