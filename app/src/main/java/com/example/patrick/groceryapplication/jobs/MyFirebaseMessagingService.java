2e90ac0f6558c62cb5e829f1fdaaa61bde2f5fce
Ben Solis <bsolis9@calstatela.edu>
2017-08-09 10:38:49 -0700

    updated to finalized push notification code

--- app/src/main/java/com/example/patrick/groceryapplication/jobs/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/patrick/groceryapplication/jobs/MyFirebaseMessagingService.java
@@ -21,7 +21,27 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage){
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            /*
+            if ( Check if data needs to be processed by long running job  true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+                handleNow();
+            }
+            */
+        }
 
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    //}
         //Generate notification
         sendNotification(remoteMessage.getNotification().getBody());
     }
